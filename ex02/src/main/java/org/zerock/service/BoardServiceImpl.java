efefd25bade437aa5a441bfed2e6f0944186f1a8
ramperrit <153475173+ramperrit@users.noreply.github.com>
2024-03-13 18:04:02 +0900

    2024.03.13

---
+++ ex02/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,51 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+
+@Service
+@RequiredArgsConstructor
+@Log4j
+public class BoardServiceImpl implements BoardService{
+	
+	private final BoardMapper mapper; //의존성 주입(@RequiredArgsConstructor)
+	
+	@Override
+	public void register(BoardVO board) {
+		log.info("register......................." + board);
+		mapper.insertSelectKey(board);
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		log.info("get.....................");
+		return mapper.read(bno);
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		log.info("modify....................." + board);
+		return mapper.update(board) == 1;
+	}
+
+	@Override
+	public boolean remove(Long bno) {
+		log.info("remove....................." + bno);
+		return mapper.delete(bno) == 1;
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		log.info("getList.....................");
+		return mapper.getList();
+	}
+	
+	
+}
---
+++ ex02/src/main/webapp/WEB-INF/views/board/list.jsp
@@ -0,0 +1,45 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+
+<%@ include file="../includes/header.jsp"%>
+
+<div class="row">
+	<div class="col-lg-12">
+		<h1 class="page-header">Tables</h1>
+	</div>
+	<!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
+<div class="row">
+	<div class="col-lg-12">
+		<div class="panel panel-default">
+			<div class="panel-heading">DataTables Advanced Tables</div>
+			<!-- /.panel-heading -->
+			<div class="panel-body">
+				<table width="100%"
+					class="table table-striped table-bordered table-hover"
+					id="dataTables-example">
+					<thead>
+						<tr>
+							<th>#No.</th>
+							<th>Title</th>
+							<th>Writer</th>
+							<th>Regdate</th>
+							<th>Update Date</th>
+						</tr>
+					</thead>
+
+				</table>
+			</div>
+			<!-- /.panel-body -->
+		</div>
+		<!-- /.panel -->
+	</div>
+	<!-- /.col-lg-6 -->
+</div>
+<!-- /.row -->
+
+<%@ include file="../includes/footer.jsp"%>
+
