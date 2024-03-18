5b43a0291a2f392ca98e57051cfbf10b24b4cc22
ramperrit <153475173+ramperrit@users.noreply.github.com>
2024-03-18 17:02:57 +0900

    2024.03.18

--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -81,6 +81,17 @@ public class BoardMapperTests {
 	}
 	
 	
+	@Test
+	public void testSearch() {
+		Criteria cri = new Criteria();
+		cri.setKeyword("테스트");
+		cri.setType("TC");
+		
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		
+		list.forEach(board -> log.info(board));
+	}
+	
 	
 	
 	
